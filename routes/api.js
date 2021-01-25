7cc8942106b07364ca538ad0bc277d46c103949e
Elyse Wells <cewells89@gmail.com>
2021-01-24 21:55:04 -0500

    console logging api transactions for testing

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
+  console.log("Hello post: api/transaction")
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-console.log("monbo", process.env.MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
